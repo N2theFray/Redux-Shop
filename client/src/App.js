6a6968efacbd42926e00e23103773e2c57e1bf7c
Scuba Steve <78673754+blacksteven1@users.noreply.github.com>
2021-07-24 18:03:51 -0400

    create store

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
-} from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
+import { ApolloProvider } from '@apollo/react-hooks'
+import { ApolloClient } from '@apollo/client';
+import { Provider } from 'react-redux'
+import store from './utils/store'
 
 import Home from './pages/Home';
 import Detail from './pages/Detail';
